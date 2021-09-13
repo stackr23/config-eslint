8e61a3b6b1d73bd1bcd364dd067a9613495edfe5
DoubleU23 <5007050+DoubleU23@users.noreply.github.com>
2021-09-13 14:24:48 +0200

    config(rules): remove deprecated TS rules

--- rules/typescript.js
+++ rules/typescript.js
@@ -1,8 +1,7 @@
 module.exports = {
   rules: {
     // additional TS rules (overrides for 'plugin:@typescript-eslint/recommended')
-    '@typescript-eslint/ban-ts-ignore':        [ 'warn' ],
-    '@typescript-eslint/camelcase':            [ 'warn' ],
+    '@typescript-eslint/ban-ts-comment':        [ 'warn' ],
     '@typescript-eslint/no-explicit-any':      [ 'warn' ],
     '@typescript-eslint/ban-types':            [ 'warn' ],
     // @typescript-eslint overrides for vanilla eslint rules
